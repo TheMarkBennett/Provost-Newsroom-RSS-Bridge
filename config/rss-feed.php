cee408a4ddb493910632f5cd044e73c929137543
Mark Bennett <mark.bennett@ucf.edu>
2020-03-02 09:54:05 -0500

    change feed urls

--- config/rss-feed.php
+++ config/rss-feed.php
@@ -14,7 +14,6 @@ add_action('init', 'ucf_provostnews_rss_feed_creator');
 function ucf_provostnews_rss_feed_creator(){
 
 
-
   $field = get_field_object('field_5e503f7bb4f52'); // get the select field with all the categories
 
   if( $field['choices'] ):
@@ -23,15 +22,15 @@ function ucf_provostnews_rss_feed_creator(){
 
           $label = strtolower(str_replace(' ', '_', $label));
 
-          $feedurl = 'pn/'. $label ; // creates the feeds url
+          $feedurl = 'pn'. $label ; // creates the feeds url
 
-           add_feed( $feedurl , 'ucf_provostnews_rss_feed_template'); //adds a new feed
+                   add_feed( $feedurl , 'ucf_provostnews_rss_feed_template'); //adds a new feed
 
       endforeach;
 
   endif;
 
-        add_feed('pn', 'ucf_provostnews_rss_feed_template'); //feed for all post
+    //  add_feed('pn', 'ucf_provostnews_rss_feed_template'); //feed for all post
 }
 
 /*
@@ -50,52 +49,50 @@ function ucf_provostnews_rss_feed_template(){
 add_action( 'pre_get_posts', 'ucf_provostnews_rss_feed_query_filter' );
 function ucf_provostnews_rss_feed_query_filter( $query ) {
 
-  if( ! $query->is_feed() && ! $query->is_main_query() || is_admin() ) { //check it the url is a feed
-
-    return;
-  }
-
-  $feedurl = $query->query["feed"]; // get the feed url
-
-  $pn_catagory = preg_replace('/^(pn)./',"", $feedurl); //remove on/ from the url
-
-  $field = get_field_object('field_5e503f7bb4f52'); // get the select field with all the categories
-
-  $pn_array = array_map('strtolower',preg_replace('/\s+/', '_', $field['choices'])); //loops through all the provost categories with regex
+    if( $query->is_feed() && $query->is_main_query() && ! is_admin() ) { //check it the url is a feed
 
 
+      $feedurl = $query->query["feed"]; // get the feed url
 
-    if ( ! in_array($pn_catagory,   $pn_array)) {
+      $pn_catagory = preg_replace('/^pn/',"", $feedurl); //remove on/ from the url
 
-        return; // if it's not one of the provost news feeds exit
-    }
+      $field = get_field_object('field_5e503f7bb4f52'); // get the select field with all the categories
+
+      $pn_array = array_map('strtolower',preg_replace('/\s+/', '_', $field['choices'])); //loops through all the provost categories with regex
 
 
+        if ( ! in_array($pn_catagory,   $pn_array)) {
 
-    if( $query->is_feed($feedurl) && $query->is_main_query() && ! is_admin() ) {
+            return; // if it's not one of the provost news feeds exit
+        }
 
-        //Get original meta query
-        $meta_query = $query->get('meta_query')? : [];
 
-         $meta_query['relation'] = 'AMD';
+            //Get original meta query
+            $meta_query = $query->get('meta_query')? : [];
 
-        //Add our meta query to the original meta queries
+             $meta_query['relation'] = 'AMD';
 
-          $meta_query[] = array(
-                              'key'=> 'ucf_provost_newsroom_checkbox',
-                              'value'=> True,
-                              'compare'=>'=',
-                          );
+            //Add our meta query to the original meta queries
 
-        $meta_query[] = array(
-                            'key'=> 'ucf_provost_newsroom_category',
-                            'value'=> $pn_catagory,
-                            'compare'=>'=',
-                        );
-          $query->set('meta_query',$meta_query);
+              $meta_query[] = array(
+                                  'key'=> 'ucf_provost_newsroom_checkbox',
+                                  'value'=> True,
+                                  'compare'=>'=',
+                              );
 
-        add_filter( 'option_posts_per_rss', 'ucf_provostnews_feed_posts_number' );
-    }
+            $meta_query[] = array(
+                                'key'=> 'ucf_provost_newsroom_category',
+                                'value'=> $pn_catagory,
+                                'compare'=>'=',
+                            );
+              $query->set('meta_query',$meta_query);
+
+            add_filter( 'option_posts_per_rss', 'ucf_provostnews_feed_posts_number' );
+
+
+      }
+
+
 
 
   }
